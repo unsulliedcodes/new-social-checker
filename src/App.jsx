9e4871758c771ac761685de8d5fd53816791a7bf
Uche / <unsulliedart24@gmail.com>
2025-12-06 14:05:51 -0800

    Refactor App component for cleaner routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom"; // No BrowserRouter here
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
@@ -11,30 +11,20 @@ import AccountVerification from "./components/AccountVerification";
 
 function App() {
   return (
-    <div className="min-h-screen flex flex-col">
+    <>
       <Header />
-      <main className="grow">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/tools" element={<Tools />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/tools/age-checker" element={<AccountChecker />} />
-          <Route path="/tools/profile-analysis" element={<ProfileAnalysis />} />
-          <Route path="/tools/verification" element={<AccountVerification />} />
-          <Route
-            path="/tools/bulk-analysis"
-            element={<div>Bulk Analysis (TODO)</div>}
-          />
-          <Route path="/blogs" element={<div>Blogs (TODO)</div>} />
-          <Route path="/platforms" element={<div>Platforms (TODO)</div>} />
-          <Route path="/contact" element={<div>Contact (TODO)</div>} />
-          <Route path="/privacy" element={<div>Privacy Policy (TODO)</div>} />
-          <Route path="/terms" element={<div>Terms of Service (TODO)</div>} />
-        </Routes>
-      </main>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/tools" element={<Tools />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/account-checker" element={<AccountChecker />} />
+        <Route path="/profile-analysis" element={<ProfileAnalysis />} />
+        <Route path="/account-verification" element={<AccountVerification />} />
+      </Routes>
       <Footer />
-    </div>
+    </>
   );
 }
 
 export default App;
+
